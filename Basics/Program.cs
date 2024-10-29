f80a48ae21dd7f6e7ed8a29426fd0638bd121aa7
student <zofia.glowacka@uczen.zsk.poznan.pl>
2024-10-29 07:27:09 +0100

    Initial commit

---
+++ Basics/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Desk d1 = new Desk();
+
+d1.colour = "green";
+d1.height = 0.5m;
+d1.brand = "Auchan";
+
+while(true)
+{
+    d1.Lift();
+    Thread.Sleep(500);
+}
+
+
+class Desk
+{
+    public const decimal MAX_HEIGHT = 2.0m;
+    public decimal height;
+    public string colour;
+    public string brand;
+
+    public void Lift()
+    {
+        if (height < MAX_HEIGHT)
+        {
+            height += 0.1m;
+        } else
+        {
+            Console.WriteLine($"Hey, it's too high. Max {MAX_HEIGHT}");
+        }
+        Console.WriteLine($"Desk is on {height} height");
+    }
+}
+
+
+// public, private, protected
+
